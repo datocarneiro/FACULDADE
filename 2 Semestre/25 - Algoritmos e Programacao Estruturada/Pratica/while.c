e02607fb0fd1fb0edbd900320cd21c3caef7d131
datocarneiro <132966071+datocarneiro@users.noreply.github.com>
2023-07-03 18:01:28 -0300

    a

--- 2 Semestre/25 - Algoritmos e Programacao Estruturada/Pratica/while.c
+++ 2 Semestre/25 - Algoritmos e Programacao Estruturada/Pratica/while.c
@@ -1,17 +1,26 @@
 #include<stdio.h>
-#include<math.h>
-#include<stdio.h>
+
+
+/* sintaxe com a repetição com teste no início:
+
+while (<condição>) 
+{
+Comando 1;
+Comando 2;
+Comando n;
+}
+*/
 
 int main()
 {
     system("cls"); // comando para limpar o terminal antes de compilar
 
-    int variavel = 0; // atenção, se a variavel nesse código for maior que 10 ficaria em loop infinito........
+    int variavel = 0; // atenção, se a variavel (contador) nesse código for maior que 10 ficaria em loop infinito........
 
-    while (variavel <10) // Enquanto a varialvel for menor que 10, irá somar a variavel mais +1. 
+    while (variavel <10) // Enquanto a condição <10 for True, vai repetir o laço, ou seja Enquanto a varialvel for menor que 10, irá somar a variavel mais +1. 
     {
-        printf("variavel atual menos um: %d \n", variavel);
-        variavel ++; 
+        printf("enquanto a variavel atual for menor que 10 pegar e somar ao incremnto: %d \n", variavel);
+        variavel ++; // incremento para não ficar em loop infinito
     }
     return 0;
 }
