24146d586f693c3c8ab66416a825f223ae5ec6cd
datocarneiro <132966071+datocarneiro@users.noreply.github.com>
2023-07-03 17:51:19 -0300

    a

--- 2 Semestre/25 - Algoritmos e Programacao Estruturada/Pratica/estruturapronta.c
+++ 2 Semestre/25 - Algoritmos e Programacao Estruturada/Pratica/estruturapronta.c
@@ -1,6 +1,18 @@
+
+/* 
+stdio.h: Fornece funções para entrada e saída padrão, como leitura e gravação de arquivos.
+math.h: funções matemáticas avançadas
+string.h: manipulação de strings, como cópia, concatenação e comparação.
+stdlib.h: alocação de memória
+time.h: funções relacionadas ao tempo e data
+ctype.h: manipular caracteres, como verificações de letras maiúsculas/minúsculas e conversões.
+*/
+
 #include<stdio.h>
 #include<math.h>
 #include<stdio.h>
+
+
 
 int main()
 {
