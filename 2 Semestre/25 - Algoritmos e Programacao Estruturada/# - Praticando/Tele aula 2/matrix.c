1afb9494cadd82830a2130deb425f68b6065c141
datocarneiro <132966071+datocarneiro@users.noreply.github.com>
2023-07-06 17:45:38 -0300

    a

--- 2 Semestre/25 - Algoritmos e Programacao Estruturada/# - Praticando/Tele aula 2/matrix.c
+++ 2 Semestre/25 - Algoritmos e Programacao Estruturada/# - Praticando/Tele aula 2/matrix.c
@@ -3,7 +3,12 @@
 int main()
 {
     system("cls"); // comando para limpar o terminal antes de compilar
-
+    int matrix [3][3];
+    for ( i = 0; i < 3; i++)
+    {
+        matrix[i] = i * 10;
+    }
+    
 
 
     return 0;
---
+++ 2 Semestre/25 - Algoritmos e Programacao Estruturada/# - Praticando/Tele aula 3/funções.c
@@ -0,0 +1,17 @@
+/*SINTAXE PARA CRIAR FUNÇÕES
+
+<tipo de retorno> <nome> (<parâmetros>) 
+{
+<Comandos da função>
+ <Retorno> (não obrigatório)
+}
+
+* <tipo de retorno> – Obrigatório. Esse parâmetro indica qual o tipo de valor que a função vai retornar.
+* <nome> – Obrigatório. Parâmetro que especifica o nome que identificará a função. 
+* <parênteses depois do nome> – Obrigatório. Toda função ou
+procedimento sempre terá o nome acompanhado de parênteses.
+Por exemplo: main(), printf(), somar() etc.
+* <comandos da função> – Opcional. Veremos na próxima seção.
+* <retorno> – Quando o tipo de retorno for void esse parâmetro não
+precisa ser usado, porém, quando não for void é obrigatório. 
+*/
