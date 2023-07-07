29a7fc63ef32e3e97336543fc6b4401ba899e1ad
datocarneiro <132966071+datocarneiro@users.noreply.github.com>
2023-07-06 23:53:45 -0300

    a

--- 2 Semestre/22 - Linguagem de Programacao/# Praticando/Ta1.py
+++ 2 Semestre/22 - Linguagem de Programacao/# Praticando/Ta1.py
@@ -3,8 +3,10 @@ variavel é alteravel
 constante não muda
 '''
 
-nome = "Jonathas"
+numero = int(input('digite o numero:'))
 
-for i, d in enumerate(nome):
-    print(f"posição= {i} , valor é: {d} ") 
-  
+for i in range(11):
+    resultado = i * numero
+    print(f"{i} x {numero} = {resultado}")
+    
+
